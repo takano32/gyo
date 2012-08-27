acf0c9c35d56d3b299cea740b3f4008ab31afffe
TAKANO Mitsuhiro <takano32@gmail.com>
2012-08-27 17:17:11 +0900

    use StreamReader

--- gyo.fsx
+++ gyo.fsx
@@ -1,4 +1,15 @@
 (* gyo.fsx *)
 
-let _ = printf "hoge"
+open System
+open System.IO
+
+let readLines filePath = seq {
+  (* StreamReader(path: string) : unit *)
+  use sr = new StreamReader (filePath: string)
+  while not sr.EndOfStream do
+    yield sr.ReadLine ()
+}
+
+let lines = readLines "TESTDATA"
+let lengths = Seq.map String.length lines
 
